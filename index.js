da574b7f585aac63ee8a7918769cd6b3645b983a
TheRealKevBot <Kglas001@gmail.com>
2021-06-09 13:07:14 -0600

    First

---
+++ index.js
@@ -0,0 +1,12 @@
+//  Digital root is the recursive sum of all the digits in a number.
+
+//  DigitalGiven n, take the sum of the digits of n. 
+//  If that value has more than one digit, continue reducing in this way until a single-digit number is produced. 
+//  The input will be a non-negative integer.
+
+function digital_root(n) {
+    
+}
+
+console.log(digital_root(16))
+console.log(digital_root(456))
